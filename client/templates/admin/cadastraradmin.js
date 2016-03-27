5897d119481a010326f7d07acc94ecdff8dbd860
Acacio Oliveira <afo.fernando@gmail.com>
2016-03-27 18:00:57 -0300

    Muitas mudanças

--- client/templates/admin/cadastraradmin.js
+++ client/templates/admin/cadastraradmin.js
@@ -5,10 +5,10 @@ if (Meteor.isClient) {
 
     console.log("cadastrarAdmin.js says: " + Noticias.find().count());
 
+    var dir = "http://res.cloudinary.com/hxnp2atgn/image/upload/v1459107694/";
     $.cloudinary.config({
-        cloud_name: "hlrpvlno9"
+        cloud_name: "hu7oelzes"
     });
-
 
 
     Template.cadastrarAdmin.helpers({
@@ -27,23 +27,84 @@ if (Meteor.isClient) {
 
         },
 
-        "click button.clickme":function(){
-            self = this;
-            var files = $("input.click_target")[0].files
-            Cloudinary._upload_file(files,function(res){
-                console.log(res);
-                console.log(self);
+        "submit .js_formnoticia": function (event) {
+            event.preventDefault();
+            var textTitle = event.target.title.value;
+            var textSub_title = event.target.sub_title.value;
+            var textCategoria = event.target.category.value;
+            var textContent = event.target.content.value;
+
+            var files = [];
+            var headerfile = $("input.click_target")[0].files
+            files.push(headerfile);
+
+            var cloudinary_id;
+            _.each(files,function(file){
+                var reader = new FileReader;
+
+                reader.onload = function () {
+
+                    Meteor.call("cloudinary_upload",reader.result,function(err,res){
+                        if(err){
+                            console.log(err);
+                        }
+                        console.log("Upaloda");
+                    });
+                };
+
+                reader.readAsDataURL(file);
             });
+            // end of Cloudinary management of Header Image
+            Cloudinary.upload(files[0], function (res) {
+                Noticias.insert(
+                    {
+                        title: textTitle,
+                        title_sub: textSub_title,
+                        content: textContent,
+                        category: textCategoria,
+                        sub_title: "Prepare seu Pip-boy: o apocalipse nunca foi tão bom",
+                        date: new Date(),
+                        img: dir + files[0].name,
+                        numero_clicks: 0,
+                        gostei: 0,
+                        nao_gostei: 0,
+                        comments: [],
+                        createdAt: new Date()
+                    });
+
+                console.log(res);
+            });
+
+            event.target.title.value = "";
+            event.target.sub_title.value = "";
+            event.target.category.value = "";
+            event.target.content.value = "";
+        },
+
+        "submit .js_savecategoria": function (event) {
+            event.preventDefault();
+            // Get value from form element
+            var textCategoria = event.target.inputCategoria.value;
+            if (textCategoria) {
+                Categorias.insert(
+                    {
+                        "nome_categoria": textCategoria
+                    }
+                );
+            }
+            event.target.inputCategoria.value = "";
+            $(".modal").modal('hide');
         }
     });
+
 }
 if (Meteor.isServer) {
     Meteor.startup(function () {
 
         Cloudinary.config({
-            cloud_name: 'hlrpvlno9',
-            api_key: '233981594891625',
-            api_secret: '95VDJfIMfT_10QuSBvqkoAl24xc'
+            cloud_name: 'hu7oelzes',
+            api_key: '935797948774166',
+            api_secret: 'Q7FcdhYJlWNi_3oMIAeNl0pifE0'
         });
 
     });
